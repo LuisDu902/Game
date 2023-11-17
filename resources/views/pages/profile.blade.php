516d25e43fbd4498c82f561872b5e98a72b0b15b
Catarina Canelas <up202103628@fe.up.pt>
2023-11-17 01:21:05 +0000

    edit profile frontend
    edit profile with three buttons: edit, save and cancel changes buttons
    frontend

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -13,6 +13,8 @@
             <li><a href="#">User Profile</a></li>
             <li> {{ Auth::user()->username }} </li>
         </ul>
+
+
     </div>
 
     <article class="profile-wrapper">
@@ -29,35 +31,44 @@
 
         </div>
 
-        <form class="profile-right">
+        <form id="profileForm" class="profile-right">
             <div class="profile-input">
                 <label class="field-label" for="name"> Name <span
                         class="purple">*</span> </label>
-                <input type="text" name="name" placeholder="{{ Auth::user()->name }}"
+                <input type="text" name="name" value="{{ Auth::user()->name }}" placeholder="{{ Auth::user()->name }}"
                     required disabled>
             </div>
 
             <div class="profile-input">
                 <label class="field-label" for="username"> Username
                     <span class="purple">*</span> </label>
-                <input type="text" name="username" placeholder="{{ Auth::user()->username }}"
+                <input type="text" name="username" value="{{ Auth::user()->username }}" placeholder="{{ Auth::user()->username }}"
                     required disabled>
             </div>
 
             <div class="profile-input email">
                 <label class="field-label" for="email"> Email Address
                     <span class="purple">*</span> </label>
-                <input type="email" name="email"
+                <input type="email" name="email" value="{{ Auth::user()->email }}"
                     placeholder="{{ Auth::user()->email }}" required disabled>
             </div>
             <div class="profile-input description">
                 <label class="field-label" for="description">
                     Description </label>
-                <textarea name="description"
+                <textarea name="description" value="{{ Auth::user()->description }}"
                     placeholder="{{ Auth::user()->description }}"
                     required disabled></textarea>
             </div>
         </form>
 
     </article>
+
+    <div class="edit-profile-button">
+        <button class="edit-button" onclick="toggleEdit()">Edit my Profile</button>
+    </div>
+
+    <div class="edit-profile-buttons d-none mx-auto">
+        <button class="save-button" onclick="saveChanges()">Save</button>
+        <button class="cancel-button" onclick="cancelChanges()">Cancel</button>
+    </div>
 @endsection
