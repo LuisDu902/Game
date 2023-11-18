6054ca2e5475cfbeb24bdd989ef54df3815f0d5d
Catarina Canelas <up202103628@fe.up.pt>
2023-11-18 20:33:30 +0000

    controller function to edit a profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\View\View;
+
+use App\Models\User;
+
+use App\Policies\UserPolicy;
 
 class UserController extends Controller
 {
@@ -19,6 +25,25 @@ class UserController extends Controller
     public function showUserProfile()
     {
         return view('pages.profile');
+    }
+
+    public function edit(Request $request)
+    {
+      $user = User::find($request->id);
+      $this->authorize('edit', [$user, Auth::user()]);
+      $user->name = $request->input('name');
+      $user->username = $request->input('username');
+
+
+      if($request->input('email') != ""){
+        $user->email = $request->input('email');
+      }
+
+      $user->description = $request->input('description');
+
+
+      $user->save();
+      return redirect()->route('profile');
     }
 
     
