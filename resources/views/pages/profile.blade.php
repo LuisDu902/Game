7fa1fed9a293a0989cee3780a5ecb8bc8edccefb
Catarina Canelas <up202103628@fe.up.pt>
2023-11-19 03:49:16 +0000

    linked the user profile in the "User Profile" of the breadcrumb

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -6,7 +6,7 @@
         <li><a href="{{ route('home') }}">
             <ion-icon name="home-outline"></ion-icon> Home</a>
         </li>
-        <li><a href="#">User Profile</a></li>
+        <li><a href="{{ route('profile', ['id' => $user->id]) }}">User Profile</a></li>
         <li> {{ $user->username }} </li>
     </ul>
 
