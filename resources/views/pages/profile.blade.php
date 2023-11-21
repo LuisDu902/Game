45b93686c30c99e11e53a4115c15753ce5b488f7
Catarina Canelas <up202103628@fe.up.pt>
2023-11-21 02:19:44 +0000

    profile page frontend changes
    - now the badges of users are shown
    - added buttons to view the questions and answers of a user in its profile page
    - rank is shown to every user

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -17,14 +17,24 @@
 
             <div class="stats-grid">
                 <div class="stats-text">Rank:</div>
-                <div class="user-rank">{{ Auth::user()->rank }}</div>
+                <div class="user-rank">{{ $user->rank }}</div>
                 <div class="stats-text">Badges:</div>
-                <div class="user-badges">{{ Auth::user()->badges }}</div>
+                <div class="user-badges">
+                    @if($badges && count($badges) > 0)
+                        <ul>
+                            @foreach($badges as $badge)
+                                <li>{{ $badge->name }}</li>
+                            @endforeach
+                        </ul>
+                    @else
+                        <p>No badges</p>
+                    @endif
+                </div>
             </div>
 
         </div>
 
-        <form id="profileForm" class="profile-right" method="POST" action="{{ route('edit_profile', ['id' => Auth::user()->id]) }}" enctype="multipart/form-data">
+        <form id="profileForm" class="profile-right" method="POST" action="{{ route('edit_profile', ['id' => $user->id]) }}" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="profile-input">
                 <label class="field-label" for="name"> Name <span
@@ -60,9 +70,17 @@
             <button class="edit-button" onclick="toggleEdit()">Edit my Profile</button>
         </div>
 
-        <div class="edit-profile-buttons d-none mx-auto">
+        <div class="edit-profile-buttons">
             <button class="save-button" type="submit" id="edit_profile" onclick="saveChanges()">Save</button>
             <button class="cancel-button" onclick="cancelChanges()">Cancel</button>
         </div>
+
+    @else
+        <div class="user-questions-answers-profile d-flex flex-row gap-4 mx-auto">
+            <a href="{{ route('users_questions', ['id' => $user->id]) }}" class="purple"> {{ $user->username }} questions </a>
+            <div class="vl"></div>
+            <a href="{{ route('users_questions', ['id' => $user->id]) }}" class="purple"> {{ $user->username }} answers </a>
+        </div>
     @endif
+
 @endsection
