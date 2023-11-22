743700411217111cb51bdfe1066d9f195fac3d2e
Ana Sofia <ana.sofia.azevedo.42@gmail.com>
2023-11-22 13:28:59 +0000

    Corrections to the new question page

--- resources/views/pages/questions.blade.php
+++ resources/views/pages/questions.blade.php
@@ -20,6 +20,47 @@
             @include('partials._questions')
         <div>
     </section>
+
+    <div id="loginModal" class="modal">
+        <div class="modal-content">
+            <div class="title-section">
+                <span class="close">&times;</span>
+                <h2>Authentication required</h2>
+            </div>
+            <p>Please log in to ask a question.</p>
+            <div class="loginModalButtons">
+                <a href="{{ route('register') }}">
+                    <button class="sign-up-btn-modal">Sign Up</button></a>
+                <a href="{{ route('login') }}">
+                    <button class="sign-in-btn-modal">Sign In</button></a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#newQuestion').click(function(event) {
+                if (!userIsLoggedIn()) {
+                    event.preventDefault();
+                    
+                    $('#loginModal').css('display', 'block');
+                }
+            });
+
+            function userIsLoggedIn() {
+                return {{ Auth::check() ? 'true' : 'false' }};
+            }
+
+            $('.close').click(function() {
+                $('#loginModal').css('display', 'none');
+            });
+
+            $(window).click(function(event) {
+                if (event.target == $('#loginModal')[0]) {
+                    $('#loginModal').css('display', 'none');
+                }
+            });
+        });
+    </script>
 @endsection
-
-
