fc7b00cddb5c316c28729a54108a399462cfcb51
Catarina Canelas <up202103628@fe.up.pt>
2023-11-23 22:18:46 +0000

    javascript to delete an answer

--- public/js/answer.js
+++ public/js/answer.js
@@ -11,3 +11,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 });
+
+
+function deleteAnswer(answerId){
+    if (confirm('Are you sure you want to delete this question?')) {
+
+        // Use the sendAjaxRequest function to send a DELETE request
+        sendAjaxRequest('DELETE', '/api/answers/' + answerId + '/delete', null, function () {
+            answerDeletedHandler(answerId).apply(this);
+        });
+    }
+}
+
+function answerDeletedHandler(answerId){
+    return function () {
+        console.log('Response:', this.responseText);
+
+        if (this.status === 200) {
+            console.log('Answer deleted successfully');
+            createNotificationBox('Answer deleted successfully!');
+            const questionElement = document.getElementById(answerId);
+            if (questionElement) {
+                questionElement.remove();
+            }
+        } else {
+            console.error('Answer delete failed:', this.statusText);
+        }
+    };
+}
