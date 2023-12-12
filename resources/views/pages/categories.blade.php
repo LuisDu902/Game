42709465f231142cbebb1e7b9d7f96faeefbae95
Catarina Canelas <up202103628@fe.up.pt>
2023-12-12 18:05:49 +0000

    button create new category frontend
    html code for the button with some changes in the structure of the page
    css for that button

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -15,8 +15,15 @@
         </ul>
    </div>
 
-   <div class="categories-section">
-        <h1>Game Categories</h1>
+   <section class="categories-section">
+        <div class="categories-group d-flex flex-row justify-content-between pe-4">
+            <h1>Game Categories</h1>
+            <div class="categories-action">
+                @if (Auth::user()->is_admin && !Auth::user()->is_banned)
+                    <a href="{{ route('questions.create') }}" id="newQuestion">Create New Category</a>
+                @endif
+            </div>
+        </div>
         <section class="categories-grid">
             @foreach($categories as $category)
                 <a href="{{ route('category', ['id' => $category->id]) }}" class="category-card">
@@ -25,5 +32,5 @@
                 </a>
             @endforeach
         </section>
-    </div>
+   </section>
 @endsection
