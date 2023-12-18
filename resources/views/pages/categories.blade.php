45cb673e742bd8c33f8d18bf6e4d1f8549efa5e0
Catarina Canelas <up202103628@fe.up.pt>
2023-12-18 03:02:46 +0000

    new html page for create category game page
    new page to show the form to create a new game category with title and description
    small changes in categories.php

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -20,15 +20,15 @@
             <h1>Game Categories</h1>
             <div class="categories-action">
                 @if (Auth::user()->is_admin && !Auth::user()->is_banned)
-                    <a href="{{ route('questions.create') }}" id="newQuestion">Create New Category</a>
+                    <a href="{{ route('categories.create') }}" id="newQuestion">Create New Category</a>
                 @endif
             </div>
         </div>
         <section class="categories-grid">
             @foreach($categories as $category)
-                <a href="{{ route('category', ['id' => $category->id]) }}" class="category-card">
+                <a href="{{ route('category', ['id' => $category->id]) }}" class="category-card" id="{{ $category->name }}">
                     <h2><span class="purple">{{ $category->name }}</span> games</h2>
-                    <p>{{ $category->description }}</p>
+                    <p class="text-break">{{ $category->description }}</p>
                 </a>
             @endforeach
         </section>
