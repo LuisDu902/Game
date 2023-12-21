28b3ccfab9ae5a11a10c155214f50eff7e6c18e8
LuAs Pedro Du <up202105385@fe.up.pt>
2023-12-21 23:26:23 +0000

    Update GameController.php

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use App\Models\GameCategory;
+use App\Models\GameMember;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
