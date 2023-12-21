d34d3f769b9276198ff86d316987556359540f76
Catarina Canelas <up202103628@fe.up.pt>
2023-12-21 01:46:09 +0000

    edit and delete tags
    icons and css

--- resources/views/partials/_editTag.blade.php
+++ resources/views/partials/_editTag.blade.php
@@ -1,6 +1,6 @@
-<div id="tag{{ $tag->id }}" class="edit-tag" data-id="{{ $tag->id }}">
+<div id="tag{{ $tag->id }}" class="edit-tag d-flex flex-row text-center" data-id="{{ $tag->id }}">
     <input class="col-8" type="text" value="{{ $tag->name }}">
-    <button class="cancel" onclick="restoreTag()">Cancel</button>
-    <button class="confirm" onclick="updateTag()">Confirm</button>
+    <ion-icon name="close-circle-outline" class="cancel text-center" onclick="restoreTag()"></ion-icon>
+    <ion-icon name="checkmark-circle-outline" class="confirm text-center" onclick="updateTag()"></ion-icon>
 </div>
     
